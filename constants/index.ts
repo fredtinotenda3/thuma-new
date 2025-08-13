66dfa2311d43bf5f5781c7f4413dae38b307b89e
Tinotenda Fred <fredtinotenda3@gmail.com>
2025-08-13 16:21:42 +0200

    Implement: SignUp Frontend

--- constants/index.ts
+++ constants/index.ts
@@ -76,14 +76,14 @@ export const onboarding = [
     id: 1,
     title: "The perfect ride is just a tap away!",
     description:
-      "Your journey begins with Ryde. Find your ideal ride effortlessly.",
+      "Your delivery begins with Thuma. Find your ideal ride effortlessly.",
     image: images.onboarding1,
   },
   {
     id: 2,
-    title: "Best car in your hands with Ryde",
+    title: "Best Bike in your hands with Thuma Mina",
     description:
-      "Discover the convenience of finding your perfect ride with Ryde",
+      "Discover the convenience of finding your perfect delivery with Thuma",
     image: images.onboarding2,
   },
   {
